0bc28faac1686ccefe2e487fb560f31fc72ce82e
Whizzpered <wizzp.prod@gmail.com>
2017-02-17 16:36:58 +0300

    Initial commit

---
+++ core/src/com/whizzpered/game/stages/Game.java
@@ -0,0 +1,13 @@
+package com.whizzpered.game.stages;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Whizzpered on 16.02.2017.
+ */
+
+public class Game extends Stage{
+
+
+
+}
