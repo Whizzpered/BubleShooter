a7abe146e3a34da8d92f62f55afcfc8f6c885c68
Whizzpered <you@example.com>
2017-02-18 00:12:12 +0300

    Let the Game begin, i think :\

--- core/src/com/whizzpered/game/MyGdxGame.java
+++ core/src/com/whizzpered/game/MyGdxGame.java
@@ -3,11 +3,8 @@ package com.whizzpered.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import com.whizzpered.game.stages.Game;
 
 public class MyGdxGame extends ApplicationAdapter {
